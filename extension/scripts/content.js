c6c14caaa4fad9246254d5e433c4a1265780e5da
complay11 <84672664+complay11@users.noreply.github.com>
2023-03-16 18:35:57 +0100

    WEBSITE?!?!??!?!?!

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -4,8 +4,22 @@ function getURL() {
     return window.location.href;
 }
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
 // working
+
+let url = getURL()
 
 document.body.remove();
 window.close(``, `_parent`, ``);
 window.open("//blocked.html");
+
+sleep(2000);
+
+document.getElementById("blockreason").innerHTML = "susmic";
